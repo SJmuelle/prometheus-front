2bc23c216eca69af20370cb951e527abfd0e5b6a
Sergio C <scortes@fintra.co>
2022-05-03 08:38:54 -0500

    cambios para montar a prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,18 +2,19 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    urlApi: 'http://demo.fintra.co:8010/api-fintra/api',
-    urlApi2: 'http://demo.fintra.co:8010/api-fintra/api/generic/qry/',
-    urlApi3: 'http://demo.fintra.co:8010/api-fintra/api/generic',
-    urlApiNgrok: 'http://demo.fintra.co:8010/api-fintra/api',
+    urlApi: 'https://prometheus.fintra.co:8443/api-fintra/api',
+    urlApi2: 'https://prometheus.fintra.co:8443/api-fintra/api/generic/qry/',
+    urlApi3: 'https://prometheus.fintra.co:8443/api-fintra/api/generic',
+    urlApiNgrok: 'https://ultracem.ngrok.io/api-fintra/api',
 
-    urlApi4: 'http://demo.fintra.co:8010/api-fintra/api/credito/tk/recursos/',
-    urlApi5: 'http://demo.fintra.co:8010/api-fintra/api/generic/',
-    urlApi6: 'http://demo.fintra.co:8010/api-fintra/api/archivos/guardar/',
+    urlApi4: 'https://ultracem.ngrok.io/api-fintra/api/credito/tk/recursos/',
+    urlApi5: 'https://ultracem.ngrok.io/api-fintra/api/generic/',
+    urlApi6: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
 
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
-    adjunto:'http://demo.fintra.co:8010/api-fintra/api/pqrs',
+    adjunto:'https://prometheus.fintra.co:8443/api-fintra/api/pqrs',
     // envioCorreo:'http://192.168.140.197:8084'
     envioCorreo:'http://prometheus.fintra.co:8443/apicredit',
-    urlUltracem: 'http://demo.fintra.co:8010/api-fintra/api'
+    urlUltracem: 'https://prometheus.fintra.co:8443/api-fintra/api'
+
 };
