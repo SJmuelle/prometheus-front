4538d1fbe89a01cc5f38e9f8d3cbf50d9ae6d83c
Shirley muelle <smuelle@fintra.co>
2022-06-22 11:43:58 -0500

    cambio de enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,19 +2,19 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    urlApi: 'https://prometheus.fintra.co:8443/api-fintra/api',
-    urlApi2: 'https://prometheus.fintra.co:8443/api-fintra/api/generic/qry/',
-    urlApi3: 'https://prometheus.fintra.co:8443/api-fintra/api/generic',
-    urlApiNgrok: 'https://ultracem.ngrok.io/api-fintra/api',
+    urlApi: 'http://tefi.ngrok.io/api-fintra/api',
+    urlApi2: 'http://tefi.ngrok.io/api-fintra/api/generic/qry/',
+    urlApi3: 'http://tefi.ngrok.io/api-fintra/api/generic',
+    urlApiNgrok: 'http://tefi.ngrok.io/api-fintra/api',
 
-    urlApi4: 'https://ultracem.ngrok.io/api-fintra/api/credito/tk/recursos/',
-    urlApi5: 'https://ultracem.ngrok.io/api-fintra/api/generic/',
-    urlApi6: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
+    urlApi4: 'http://tefi.ngrok.io/api-fintra/api/credito/tk/recursos/',
+    urlApi5: 'http://tefi.ngrok.io/api-fintra/api/generic/',
+    urlApi6: 'http://tefi.ngrok.io/api-fintra/api/archivos/guardar/',
 
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
-    adjunto:'https://prometheus.fintra.co:8443/api-fintra/api/pqrs',
+    adjunto:'http://tefi.ngrok.io/api-fintra/api/pqrs',
     // envioCorreo:'http://192.168.140.197:8084'
     envioCorreo:'http://prometheus.fintra.co:8443/apicredit',
-    urlUltracem: 'https://prometheus.fintra.co:8443/api-fintra/api'
+    urlUltracem: 'http://tefi.ngrok.io/api-fintra/api'
 
 };
