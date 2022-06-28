a7549117a8fb0629b2c784253d5ef58c7e45c1f5
tavosalas27 <tavosalas270@gmail.com>
2022-06-28 08:29:10 -0500

    Actualizacion de environment en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
     userName: "APPWEB",
     password: btoa("123456"),
     urlApi: 'https://prometheus.fintra.co:8443/api-fintra/api',
-    urlApi2: 'https://prometheus.fintra.co:8443/api-fintra/api/generic/qry/',
-    urlApi3: 'https://prometheus.fintra.co:8443/api-fintra/api/generic',
+    urlApi2: 'http://demo.fintra.co:8010/api-pqrs/api/generic/qry/',
+    urlApi3: 'http://demo.fintra.co:8010/api-pqrs/api/generic',
     urlApiNgrok: 'https://ultracem.ngrok.io/api-fintra/api',
 
     urlApi4: 'https://ultracem.ngrok.io/api-fintra/api/credito/tk/recursos/',
@@ -14,7 +14,7 @@ export const environment = {
     urlPagaduria: 'https://prometheus.fintra.co:8443/pagadurias/',
 
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
-    adjunto:'https://prometheus.fintra.co:8443/api-fintra/api/pqrs',
+    adjunto:'http://demo.fintra.co:8010/api-pqrs/api/pqrs',
     // envioCorreo:'http://192.168.140.197:8084'
     envioCorreo:'http://prometheus.fintra.co:8443/apicredit',
     urlUltracem: 'https://prometheus.fintra.co:8443/api-fintra/api'
