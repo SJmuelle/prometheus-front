a7b2b875662ed216c5285787fc4542d2a991597b
Shirley muelle <smuelle@fintra.co>
2022-07-05 15:27:05 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,6 +10,7 @@ export const environment = {
     urlApi4: 'http://demo.fintra.co:8011/api-fintra/api/credito/tk/recursos/',
     urlApi5: 'http://demo.fintra.co:8011/api-fintra/api/generic/',
     urlApi6: 'http://demo.fintra.co:8011/api-fintra/api/archivos/guardar/',
+    urlPagaduria: 'http://demo.fintra.co:8010/pagadurias/',
 
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
     adjunto:'http://demo.fintra.co:8011/api-fintra/api/pqrs',
