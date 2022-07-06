fb34589f0fa601e7396855344672632d6dd5b780
Shirley muelle <smuelle@fintra.co>
2022-07-06 12:07:18 -0500

    cambios de agenda de trazabilidad

--- src/app/modules/admin/apps/fabrica-credito/trazabilidad-credito/detalle-trazabilidad/detalle-trazabilidad.component.ts
+++ src/app/modules/admin/apps/fabrica-credito/trazabilidad-credito/detalle-trazabilidad/detalle-trazabilidad.component.ts
@@ -18,6 +18,7 @@ export class DetalleTrazabilidadComponent implements OnInit {
   public identificacion: string = this.route.snapshot.paramMap.get('id');
   dataPolicitasAdmin: any;
   datos2: any[];
+  datosGeneral:any[];
   verComentarios: boolean = false;
   constructor(
     private _fabricaCreditoService: FabricaCreditoService,
@@ -37,29 +38,25 @@ export class DetalleTrazabilidadComponent implements OnInit {
  * @description: Obtiene los datos del resumen
  */
   private getResumen(): void {
-
     Swal.fire({ title: 'Cargando', html: 'Buscando información...', timer: 500000, didOpen: () => { Swal.showLoading() }, }).then((result) => { });
     let data = {
       "numeroSolicitud": this.numeroSolicitud,
       "identificacion": this.identificacion
     }
     this._fabricaCreditoService.getResumenCredito(data).pipe(
-
     ).subscribe((res) => {
+      Swal.close();
       if (res.status === 200) {
-        console.log(res.data)
+        debugger
         this.dataResumenTrazabilidad = res.data.resumenTrazabilidad;
-
-        // this.getDatos(res.data);
-        Swal.close();
+        this.getDatos(res.data)
       } else {
         this.dataResumenTrazabilidad = [];
-
-        Swal.close();
-        // this.datos = []
+        this.datosGeneral=[];
       }
     });
   }
+  
   /**
  * @description: Obtiene los datos del trazabilidad
  */
@@ -72,7 +69,7 @@ export class DetalleTrazabilidadComponent implements OnInit {
     ).subscribe((res) => {
       if (res.status === 200) {
         console.log(res.data)
-        this.datos = data;
+        this.datos = res.data;
         Swal.close();
       } else {
         Swal.close();
@@ -83,10 +80,56 @@ export class DetalleTrazabilidadComponent implements OnInit {
 
   private getDatos(data): void {
 
-    // debugger;
-    this.datos = data;
+    //general
+    let DatosCredito = [];
+    DatosCredito.push(
+      {
+        titulo: "Información del cliente:",
+        tipo: "campos",
+        icono: "feather:user",
+        color: "text-blue-600",
+        descripcion: "Datos específicos del cliente:",
+        campos: [
+          {
+            icono: "heroicons_outline:user-circle",
+            color: "bg-blue-100 text-blue-800",
+            label: "Cliente:",
+            valor: data.resumenCredito.nombreCompleto.toLowerCase(),
+            valor2: data.resumenCredito.tipoDocumento + '-' + data.resumenCredito.identificacion
+          },
+          {
+            icono: "feather:dollar-sign",
+            color: "bg-green-100 text-green-800",
+            label: "Crédito: ",
+            valor: "$" + data.resumenCredito.cupoTotal,
+            valor2: "Plazo: " + data.resumenCredito.plazo + " meses" ,
+            valor3: "Tasa: " + data.resumenCredito.tasa + "%"
 
-
+          },
+          {
+            icono: "heroicons_outline:light-bulb",
+            color: "bg-pink-100 text-pink-800",
+            label: "Condiciones: ",
+            valor: "Ren: " + data.resumenCredito.renovacion,
+            valor2: "Seg: " + data.resumenCredito.segmento
+          },
+          {
+            icono: "heroicons_outline:office-building",
+            color: "bg-gray-100 text-gray-800",
+            label: "Empresa",
+            valor: data.resumenGeneral.agenda,
+            valor2: data.resumenGeneral.asesorComercial
+          },
+          {
+            icono: "mat_outline:location_on",
+            color: "bg-purple-100 text-purple-800",
+            label: "Ubicación: ",
+            valor: data.resumenCredito.departamentoVivienda.toLowerCase() + ", " + data.resumenCredito.ciudadVivienda.toLowerCase()
+          }
+        ]
+      },
+    )
+    this.datosGeneral = DatosCredito;
   }
 
   /**
