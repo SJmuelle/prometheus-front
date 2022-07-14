4f804ddab4a0f2cc4eb5cc89dbd5a3930ec57bf9
tavosalas27 <tavosalas270@gmail.com>
2022-07-14 18:06:56 -0500

    Listos los environment para test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,21 +2,21 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    urlApi: 'https://prometheus.fintra.co:8443/api-fintra/api',
+    urlApi: 'http://demo.fintra.co:8010/api-fintra/api',
     urlApi2: 'http://demo.fintra.co:8010/api-pqrs/api/generic/qry/',
     urlApi3: 'http://demo.fintra.co:8010/api-pqrs/api/generic',
     urlApiNgrok: 'https://ultracem.ngrok.io/api-fintra/api',
 
-    urlApi4: 'https://ultracem.ngrok.io/api-fintra/api/credito/tk/recursos/',
-    urlApi5: 'https://ultracem.ngrok.io/api-fintra/api/generic/',
-    urlApi6: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
+    urlApi4: 'http://demo.fintra.co:8010/api-fintra/api/credito/tk/recursos/',
+    urlApi5: 'http://demo.fintra.co:8010/api-fintra/api/generic/',
+    urlApi6: 'http://demo.fintra.co:8010/api-fintra/api/archivos/guardar/',
 
-    urlPagaduria: 'https://prometheus.fintra.co:8443/',
+    urlPagaduria: 'http://demo.fintra.co:8010/',
 
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
     adjunto:'http://demo.fintra.co:8010/api-pqrs/api/pqrs',
     // envioCorreo:'http://192.168.140.197:8084'
-    envioCorreo:'http://prometheus.fintra.co:8443/apicredit',
-    urlUltracem: 'https://prometheus.fintra.co:8443/api-fintra/api'
+    envioCorreo:'http://demo.fintra.co:8010/apicredit',
+    urlUltracem: 'http://demo.fintra.co:8010/api-fintra/api'
 
 };
