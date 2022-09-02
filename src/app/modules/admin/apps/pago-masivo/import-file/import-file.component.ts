84a5ee53284847e3ad46743f00f699fef75fde0f
tavosalas27 <tavosalas270@gmail.com>
2022-09-02 15:25:44 -0500

    En proceso la vista dinamica

--- src/app/modules/admin/apps/pago-masivo/import-file/import-file.component.ts
+++ src/app/modules/admin/apps/pago-masivo/import-file/import-file.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -9,7 +9,11 @@ import * as XLSX from 'xlsx';
 export class ImportFileComponent implements OnInit {
 
   listRowsExcel: any = []; // filas del archivo convertido a JSON
+  cabeceras: any = []
+  valores: any = []
+  listValores: any = []
   filtrarTabla:string=''; // filtrar la tabla
+  @ViewChild('fileInput') fileInput: ElementRef;
 
   constructor() { }
 
@@ -29,7 +33,20 @@ export class ImportFileComponent implements OnInit {
       let hoja = workbook.SheetNames[0];
       const data = XLSX.utils.sheet_to_json(workbook.Sheets[hoja]);
       this.listRowsExcel = data;
-      console.log(this.listRowsExcel)
+      // console.log(this.listRowsExcel)
+      this.cabeceras=Object.keys(this.listRowsExcel[0])
+      // console.log(this.cabeceras)
+
+      for (let index = 0; index < this.listRowsExcel.length; index++) {
+        const element = this.listRowsExcel[index];
+        this.valores = Object.values(element)
+        // console.log(this.valores)
+        // const obj = Object.assign({}, this.valores);
+        // console.log(obj)
+        this.listValores.push(this.valores)
+      }
+      console.log(this.listValores)
+      console.log(this.listValores[0])
       // workbook.SheetNames.forEach(sheet => {
       //   const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
       //   this.createdJson = JSON.stringify(data, undefined, 4);
@@ -37,6 +54,7 @@ export class ImportFileComponent implements OnInit {
       // })
       // console.log(workbook.SheetNames[0])
     }
+    this.fileInput.nativeElement.value = '';
   }
 
 }
