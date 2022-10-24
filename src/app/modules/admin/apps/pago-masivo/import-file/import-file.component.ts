935f55b37f3150e930e43d1e06b9f4fe7ac0aee8
tavosalas27 <tavosalas270@gmail.com>
2022-10-24 11:05:02 -0500

    Realizado el funcionamiento por medio de cheks al subir el archivo

--- src/app/modules/admin/apps/pago-masivo/import-file/import-file.component.ts
+++ src/app/modules/admin/apps/pago-masivo/import-file/import-file.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as XLSX from 'xlsx';
+import moment from 'moment';
 
 @Component({
   selector: 'app-import-file',
@@ -9,9 +10,8 @@ import * as XLSX from 'xlsx';
 export class ImportFileComponent implements OnInit {
 
   listRowsExcel: any = []; // filas del archivo convertido a JSON
-  cabeceras: any = []
-  valores: any = []
-  listValores: any = []
+  cabeceras: any = [];
+  listAsignados: any = {"details":[]};
   filtrarTabla:string=''; // filtrar la tabla
   chequeada: boolean = false;
   @ViewChild('fileInput') fileInput: ElementRef;
@@ -26,7 +26,6 @@ export class ImportFileComponent implements OnInit {
   subirArchivo(event) {
     this.listRowsExcel = [];
     this.cabeceras = [];
-    this.listValores = [];
     const seleccionado = event.target.files[0];
     const lector = new FileReader();
     lector.readAsBinaryString(seleccionado);
@@ -36,37 +35,67 @@ export class ImportFileComponent implements OnInit {
       let hoja = workbook.SheetNames[0];
       const data = XLSX.utils.sheet_to_json(workbook.Sheets[hoja]);
       this.listRowsExcel = data;
-      console.log(this.listRowsExcel);
       this.cabeceras=Object.keys(this.listRowsExcel[0])
       for (let index = 0; index < this.listRowsExcel.length; index++) {
         const element = this.listRowsExcel[index];
-        this.valores = Object.values(element)
-        this.listValores.push(this.valores)
+        element['nitTransportadora']=element['Identificacion'];
+        delete element['Identificacion'];
+        element['fechaPago']=element['Fecha Pago'];
+        delete element['Fecha Pago'];
+        element['banco']=element['Banco'];
+        delete element['Banco'];
+        element['sucursal']=element['Sucursal'];
+        delete element['Sucursal'];
+        element['valorAplicar']=element['Valor Aplicar Neto'];
+        delete element['Valor Aplicar Neto'];
+        element['comisionRecaudo']=element['Comision Recaudo'];
+        delete element['Comision Recaudo'];
+        element.chequeado = false;
       }
-      this.listValores['chequeado'] = false;
-      console.log(this.listValores)
     }
     this.fileInput.nativeElement.value = '';
   }
 
   updateAllComplete() {
-    this.chequeada = this.listValores != null && this.listValores.every(t => (t.chequeado));
+    this.chequeada = this.listRowsExcel != null && this.listRowsExcel.every(t => (t.chequeado));
   }
 
   setAll(completed: boolean) {
     this.chequeada = completed;
-    if (this.listValores == null) {
+    if (this.listRowsExcel == null) {
       return;
     }
-    this.listValores.forEach(t => (t.chequeado = completed));
+    this.listRowsExcel.forEach(t => (t.chequeado = completed));
   }
 
+  acumular(item, event){
+    let data = {
+      "nitTransportadora": item.nitTransportadora,         
+      "fechaPago": moment(item.fechaPago).format("YYYY-MM-DD"),
+      "banco": item.banco,
+      "sucursal": item.sucursal,
+      "valorAplicar":item.valorAplicar.toString(),
+      "comisionRecaudo":item.comisionRecaudo.toString()
+    }
 
+    if (event.checked) {
+      this.listAsignados.details.push(data)
+    } else {
+      const dataBuscar = this.listAsignados.details.filter(data => data.nitTransportadora == item.nitTransportadora);
+      let idx = this.listAsignados.details.indexOf(dataBuscar[0]);
+      this.listAsignados.details.splice(idx, 1);
+    }
 
-  acumular(item, index, chequeado){
-    console.log(index)
-    console.log(item)
-    console.log(chequeado)
+    if (this.listAsignados.length >= this.listRowsExcel.length) {
+      this.chequeada = true;
+    } else {
+      this.chequeada = false;
+    }
+
+  }
+
+  enviar(){
+
   }
 
 }
