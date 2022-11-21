b9b4f14e6d0bff6d8483098819b629bfc79dc3b3
tavosalas27 <tavosalas270@gmail.com>
2022-11-21 11:52:21 -0500

    Se actualiza el environment para compilado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    apiUrl:"http://demo.fintra.co:8010/",
+    apiUrl:"http://demo.fintra.co:8011/",
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
 };
