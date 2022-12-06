1a16a7fe60b45126fee9a989e2dc5ff0b48283a0
Edgar Gonzalez Mendoza <egonzalez@fintra.co>
2022-12-06 09:18:35 -0500

    cambios sin commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    apiUrl:"https://prometheus.fintra.co:8443/",
-    urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
+  //  apiUrl:"https://prometheus.fintra.co:8443/",
+   // urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
+    apiUrl:"http://10.209.3.5:8084/",
+    urlprometheus:'http://10.209.3.5:8094/fintra/EndPointCoreServlet',
 };
