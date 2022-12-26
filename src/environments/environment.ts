f8c59156761479e5709047e2c47226217a2a0dea
tavosalas27 <tavosalas270@gmail.com>
2022-12-26 14:52:19 -0500

    Revisadas y corregidas las demas URL de getListados en pqrs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     userName: "APPWEB",
     password: btoa("123456"),
     // apiUrl: "http://localhost:8010/",
-    apiUrl:"https://30c4-181-57-229-82.ngrok.io/",
+    apiUrl:"https://prometheus.fintra.co:8443/",
     // diego
     // apiUrl:"http://192.168.138.221:8010/",
     // apiUrl:"http://demo.fintra.co:8011/",
