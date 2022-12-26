dbf235d9449870e8a48d8ed70242f3f2bf558400
tavosalas27 <tavosalas270@gmail.com>
2022-12-26 11:24:44 -0500

    Se corrige los responsables

--- src/app/modules/admin/apps/pqr/responsables-pqrs/responsables-pqrs.component.ts
+++ src/app/modules/admin/apps/pqr/responsables-pqrs/responsables-pqrs.component.ts
@@ -117,7 +117,7 @@ export class ResponsablesPQRSComponent implements OnInit {
   consultaListado(){
     Swal.fire({ title: 'Cargando', html: 'Buscando información de Tipos de PQRS', timer: 500000, didOpen: () => { Swal.showLoading() }, }).then((result) => { })
         this._pqrService
-          .getListados(`/tk/select-areas`)
+          .getListados(`tk/select-areas`)
           .subscribe((response: any) => {
             Swal.close();
             if (response) {
