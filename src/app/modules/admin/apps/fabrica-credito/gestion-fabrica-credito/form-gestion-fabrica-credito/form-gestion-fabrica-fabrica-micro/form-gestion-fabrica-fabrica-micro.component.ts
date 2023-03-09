da49d24c65d8e87f4ac34bcbe96da7e900011f10
Pedro Munoz <62898809+SrDevDash@users.noreply.github.com>
2023-03-09 08:55:15 -0500

    hotfix
    Si no se envia fecha, se asigna una por defecto

--- src/app/modules/admin/apps/fabrica-credito/gestion-fabrica-credito/form-gestion-fabrica-credito/form-gestion-fabrica-fabrica-micro/form-gestion-fabrica-fabrica-micro.component.ts
+++ src/app/modules/admin/apps/fabrica-credito/gestion-fabrica-credito/form-gestion-fabrica-credito/form-gestion-fabrica-fabrica-micro/form-gestion-fabrica-fabrica-micro.component.ts
@@ -99,8 +99,8 @@ export class FormGestionFabricaFabricaMicroComponent implements OnInit, OnDestro
         const { numeroHijos, autorizacionBanco,telefonoNegocio, barrioResidencia, antiguedadActividad, valorSolicitado, plazo, personasACargo, fechaDesvinculacionExpuesta, fechaDesvinculacionPublico, fechaNacimiento, fechaExpedicion, estrato, ...data } = datos;
         const fechaNacimientoFormato = moment(fechaNacimiento.toString()).format('YYYY-MM-DD');
         const fechaExpedicionFormato = moment(fechaExpedicion.toString()).format('YYYY-MM-DD');
-        const fechaDesvinculacionPublicoFormato = fechaDesvinculacionPublico ? moment(fechaDesvinculacionPublico.toString()).format('YYYY-MM-DD') : "";
-        const fechaDesvinculacionExpuestaFormato = fechaDesvinculacionExpuesta ? moment(fechaDesvinculacionExpuesta.toString()).format('YYYY-MM-DD'): "";
+        const fechaDesvinculacionPublicoFormato = fechaDesvinculacionPublico ? moment(fechaDesvinculacionPublico.toString()).format('YYYY-MM-DD') : "0099-01-01";
+        const fechaDesvinculacionExpuestaFormato = fechaDesvinculacionExpuesta ? moment(fechaDesvinculacionExpuesta.toString()).format('YYYY-MM-DD'): "0099-01-01";
         const numeroHijosFormato = Number(numeroHijos);
         const barrioResidenciaFormato = Number(barrioResidencia);
         const antiguedadActividadFormato = Number(antiguedadActividad);
@@ -199,6 +199,7 @@ export class FormGestionFabricaFabricaMicroComponent implements OnInit, OnDestro
                 });
                 this.unidadNegocio = data.unidadNegocio;
                 this.fabricaDatos = data;
+                console.log(this.fabricaDatos, "fabrica Datos");
                 
             });
     }
@@ -412,7 +413,7 @@ export class FormGestionFabricaFabricaMicroComponent implements OnInit, OnDestro
             antiguedadLocal: ['', Validators.required],
             nombreArrendador: [''],
             celularArrendador: [''],
-            tipoUbicacionNegocio: ['', Validators.required],
+            tipoUbicacionNegocio: [''],
             numeroEmpleados: ['', [Validators.required]],
             nombreAtiendeNegocio: ['', Validators.required],
             tieneOtrosPuntos: ['', Validators.required],
