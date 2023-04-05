87be9d5e28590bae240b905bbee588be86f7d9e1
Pedro Munoz <62898809+SrDevDash@users.noreply.github.com>
2023-04-05 14:17:41 -0500

    change: enviroment a produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    apiUrl:"http://demo.fintra.co:8084/",
+    apiUrl:"http://demo.fintra.co:8011/",
     urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
 };
