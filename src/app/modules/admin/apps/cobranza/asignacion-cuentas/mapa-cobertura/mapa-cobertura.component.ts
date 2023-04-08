5c8ec2ef396a21f9560e3971493716a50553767f
unknown <ingshirleyjohanna@gmail.com>
2023-04-08 10:47:29 -0500

    cambios de diseño y creacion del mapa

--- src/app/modules/admin/apps/cobranza/asignacion-cuentas/mapa-cobertura/mapa-cobertura.component.ts
+++ src/app/modules/admin/apps/cobranza/asignacion-cuentas/mapa-cobertura/mapa-cobertura.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-mapa-cobertura',
   templateUrl: './mapa-cobertura.component.html',
   styleUrls: ['./mapa-cobertura.component.scss']
 })
-export class MapaCoberturaComponent implements OnInit {
+export class MapaCoberturaComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  private map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [11.016763998741775, -74.83146618188988],
+      zoom: 30
+    });
+    let circle = L.circle([11.016763998741775, -74.83146618188988], {
+      color: 'green',
+      fillColor: 'green',
+      fillOpacity: 0.5,
+      radius: 10
+    }).addTo(this.map);
+
+    let popup = L.popup()
+    .setLatLng([11.016763998741775, -74.83146618188988])
+    .setContent("holaaa")
+    .openOn(this.map);
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
+
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
