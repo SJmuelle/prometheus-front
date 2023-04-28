0abd10b644fee7cb9d987dd654d007f64a19a291
Pedro Munoz <62898809+SrDevDash@users.noreply.github.com>
2023-04-28 15:59:48 -0500

    fix: posible bug

--- src/app/modules/admin/apps/fabrica-credito/agenda-referenciacion/form-dialog-list-error-dialog/form-dialog-list-error-dialog.component.ts
+++ src/app/modules/admin/apps/fabrica-credito/agenda-referenciacion/form-dialog-list-error-dialog/form-dialog-list-error-dialog.component.ts
@@ -59,6 +59,8 @@ export class FormDialogListErrorDialogComponent implements OnInit {
                 return 'Codeudor'
             case 'S':
                 return 'Dedudor solidario'
+                default:
+                return 'Titular'
         }
     }
 
