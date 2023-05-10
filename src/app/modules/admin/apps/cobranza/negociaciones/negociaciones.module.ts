a41f79c62e34c57ea9c5882a7b6ea5edae977202
unknown <ingshirleyjohanna@gmail.com>
2023-05-10 08:47:43 -0500

    creacion del modulo negocion y subida de rpidez por el titulo de gestion de cartera

---
+++ src/app/modules/admin/apps/cobranza/negociaciones/negociaciones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NegociacionesRoutingModule } from './negociaciones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NegociacionesRoutingModule
+  ]
+})
+export class NegociacionesModule { }
