928cffd3e4e4fce158c6c4e70ff9d169141c3c84
Pedro Munoz <Pedrojose0272@gmail.com>
2023-05-17 14:31:26 -0500

    change: pagos-transportadoras nombre modulo

--- src/app/modules/admin/apps/cuentas-por-cobrar/recaudos/recaudos-routing.module.ts
+++ src/app/modules/admin/apps/cuentas-por-cobrar/recaudos/recaudos-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagoMasivoModule } from './pago-masivo/pago-masivo.module';
+import { PagoMasivoModule } from './pagos-transportadoras/pago-masivo.module';
 
 const routes: Routes = [{
   path: 'transportadora', loadChildren: ()=> PagoMasivoModule
