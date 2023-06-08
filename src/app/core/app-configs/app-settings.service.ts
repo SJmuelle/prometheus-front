fb837913898e1e4923dbdbb8126e18a11428aa68
Pedro Munoz <62898809+SrDevDash@users.noreply.github.com>
2023-06-08 14:45:48 -0500

    Revert "add: mapa primera parte micro"
    This reverts commit a726283eb94078aa0dfaa88fc7f1f6cce8337612.

--- src/app/core/app-configs/app-settings.service.ts
+++ src/app/core/app-configs/app-settings.service.ts
@@ -103,7 +103,6 @@ export class AppSettingsService {
     public formulario = {
         url: {
             // https://prometheus.fintra.co:8443/api-fintra/api/credito/tk/property/parametros-cargue-inicial
-            latitudLongitud: EndPoints.alfa('/geocoding-address'),
             microcredito: EndPoints.uri('/credito/microcredito/guardado-solicitud-microcredito'),
             cargueInicial: EndPoints.uri('/credito/tk/property/parametros-cargue-inicial'),
             cargueActividadEconomica: EndPoints.uri('/generic/qry/obtener-actividades-form-micro'),
