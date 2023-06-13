837e561bd88cae28a3cc3791eac3a22ebc4be16f
Leonardo Arape <larape@geotech.com.co>
2023-06-13 17:15:57 -0500

    add filter Table service

--- src/app/modules/admin/apps/negociaciones/negociacion-cartera/negociacion-cartera.component.ts
+++ src/app/modules/admin/apps/negociaciones/negociacion-cartera/negociacion-cartera.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NegociacionCarteraService } from 'app/core/services/negociacion-cartera.service';
+import { TableDataFilterService } from 'app/core/services/table-data-filter.service';
 
 @Component({
   selector: 'app-negociacion-cartera',
@@ -9,16 +10,22 @@ import { NegociacionCarteraService } from 'app/core/services/negociacion-cartera
 export class NegociacionCarteraComponent implements OnInit {
 
   public seacrhType: 'Cedula' | 'Negocio' = 'Cedula'
-  public valueSearch: string = '';
+  public valueSearch: string = 'MC17223';
   public dataRow: any[] = []
 
 
-  constructor(private _negociacionCarteraService: NegociacionCarteraService) { }
+  constructor(private _negociacionCarteraService: NegociacionCarteraService,
+    private _tableFilter: TableDataFilterService) { }
 
   ngOnInit(): void {
   }
 
+  public filtrar(text: string): void {
+    const filterValue = text.trim().toUpperCase();
+    this._tableFilter.sendFilterData(filterValue);
 
+
+  }
 
   public search(): void {
 
