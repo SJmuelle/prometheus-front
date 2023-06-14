a976beaf5926424ca44ad662e1c93985df5e522e
unknown <ingshirleyjohanna@gmail.com>
2023-06-14 08:09:24 -0500

    creacion del monulo de monto y plazo

--- src/app/modules/admin/apps/pagaduria/pagaduria-routing.module.ts
+++ src/app/modules/admin/apps/pagaduria/pagaduria-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
       {
         path: 'parametria',
         loadChildren: () => GestionPagaduriaModule
+      },
+      {
+        path: 'plazo',
+        loadChildren: () => GestionPagaduriaModule
       }
     ]
   }
