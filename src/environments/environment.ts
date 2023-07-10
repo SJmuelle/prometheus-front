a84a6ddb4a04fb40d7d49abdfa987e996c9cd55d
Pedro Munoz <62898809+SrDevDash@users.noreply.github.com>
2023-07-10 09:26:30 -0500

    Merge branch 'main' into pm-mejoras-micro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,9 @@ export const environment = {
     password: btoa("123456"),
     // apiUrl: "http://localhost:8010/",
     // apiUrl: "http://demo.fintra.co:8084/",
-    apiUrl: "https://prometheus.fintra.co:8443/",
+   // apiUrl: "https://prometheus.fintra.co:8443/",
     // diego
-    // apiUrl:"http://192.168.138.221:8010/",
+     apiUrl:"http://192.168.138.55:8084/",
     // apiUrl:"http://192.168.140.197:8084/",
     // apiUrl:"http://demo.fintra.co:8011/",
     alfaFintra: "http://alfa.fintra.co:3100/",
