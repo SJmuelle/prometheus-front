34ec2a46cb2506b8728853abe70a535177960061
Leonardo Arape <larape@geotech.com.co>
2023-07-19 23:00:13 -0500

    change environmetn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: true,
     userName: "APPWEB",
     password: btoa("123456"),
-    apiUrl: "http://demo.fintra.co:8084/",
+    // apiUrl: "http://demo.fintra.co:8084/",
 
-    // apiUrl:"https://prometheus.fintra.co:8443/",
-    urlprometheus:'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
+    apiUrl: "https://prometheus.fintra.co:8443/",
+    urlprometheus: 'https://prometheus.fintra.co:8444/fintra/EndPointCoreServlet',
     alfaFintra: "http://alfa.fintra.co:3100/",
     apiPrometheusViejo: "http://prometheus.fintra.co:8084/",
 };
