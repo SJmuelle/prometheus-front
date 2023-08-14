58f7b82fd6c4bd0fe4fa6e184a4882f57d0f643a
sjmuelle <ingshirleyjohanna@gmail.com>
2023-08-14 09:01:58 -0500

    creacion de lo servicios

---
+++ src/app/core/services/cobranza.service.ts
@@ -0,0 +1,116 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AppSettingsService } from '../app-configs/app-settings.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CobranzaService {
+
+  
+  private _cuentasAsignadas: BehaviorSubject<any[] | null> = new BehaviorSubject(null);
+
+
+  constructor(private _http: HttpClient, private _appSettings: AppSettingsService) { }
+
+
+  /**
+ * Getter for _cuentasAsignadas
+ */
+  get cuentasAsignadas$(): Observable<any[]> {
+    return this._cuentasAsignadas.asObservable();
+  }
+
+
+  /**
+   * @description: Obtiene listado de informacion-negocios-asignados 
+   */
+  public getInformacionNegocios(): Observable<any> {
+    return this._http.get(`${this._appSettings.cobranza.url.infoNegocio}`)
+  }
+
+
+
+  /**
+   * Get cobranza
+   */
+  getCuentasAsignadas(): Observable<any[]> {
+    return this._http.get<any>(`${this._appSettings.cobranza.url.infoNegocio}`).pipe(
+      tap((respuesta) => {
+        this._cuentasAsignadas.next(respuesta.data);
+      })
+    );
+  }
+
+  /*
+   * @description: 
+   */
+  public congelaCalculoTotal(datos: any): Observable<any> {
+    return this._http.post(this._appSettings.cobranza.url.congelaCalculoTotal, datos);
+  }
+  /*
+ * @description: 
+ */
+  public congelaCalculoDescuento(datos: any): Observable<any> {
+    return this._http.post(this._appSettings.cobranza.url.congelaCalculoDescuento, datos);
+  }
+
+  /*
+  * @description: 
+  */
+  public congelaCongelaSimulador(datos: any): Observable<any> {
+    return this._http.post(this._appSettings.cobranza.url.congelaSimulador, datos);
+  }
+
+  /*
+  * @description: 
+  */
+  public congelaCongelaGuardar(datos: any): Observable<any> {
+    return this._http.post(this._appSettings.cobranza.url.congelaGuardar, datos);
+  }
+
+  /*
+  * @description: 1
+  */
+  public refinanciacionTipoEstrategia(): Observable<any> {
+    return this._http.get(this._appSettings.cobranza.url.refinanciacionTipoEstrategia);
+  }
+
+  /*
+  * @description: 2
+  */
+  public refinanciacionBuscarCliente(tipoDato,tipoEstrategia,id): Observable<any> {
+    return this._http.get(`${this._appSettings.cobranza.url.refinanciacionBuscarCliente}/${tipoDato}/${id}/${tipoEstrategia}`);
+  }
+
+  /*
+  * @description: 
+  */
+  public refinanciacionCargarDetalleCartera(negocio,tipoEstrategia,fecha): Observable<any> {
+    return this._http.get(`${this._appSettings.cobranza.url.refinanciacionCargarDetalleCartera}/${negocio}/${tipoEstrategia}/${fecha}`);
+  }
+
+  /*
+  * @description: 
+  */
+  public refinanciacionBuscarFechaPago(): Observable<any> {
+    return this._http.get(this._appSettings.cobranza.url.refinanciacionBuscarFechaPago);
+  }
+
+
+  /*
+  * @description: 
+  */
+  public refinanciacionCalcularCtaInicial(datos: any): Observable<any> {
+    return this._http.post(this._appSettings.cobranza.url.refinanciacionCalcularCtaInicial, datos);
+  }
+
+  /*
+  * @description: 
+  */
+  public refinanciacionCargarDtoCongela(): Observable<any> {
+    return this._http.get(this._appSettings.cobranza.url.refinanciacionCargarDtoCongela);
+  }
+}
