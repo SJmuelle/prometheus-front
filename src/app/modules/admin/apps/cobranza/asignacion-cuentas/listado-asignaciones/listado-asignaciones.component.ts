f3f327ab0f5f4cf09a19c4066d46c365d02f2b3d
sjmuelle <ingshirleyjohanna@gmail.com>
2023-08-15 20:01:05 -0500

    cambios de asignacion de cuenta

--- src/app/modules/admin/apps/cobranza/asignacion-cuentas/listado-asignaciones/listado-asignaciones.component.ts
+++ src/app/modules/admin/apps/cobranza/asignacion-cuentas/listado-asignaciones/listado-asignaciones.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CobranzaService } from 'app/core/services/cobranza.service';
+import { TableDataFilterService } from 'app/core/services/table-data-filter.service';
 
 @Component({
   selector: 'listado-asignaciones',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoAsignacionesComponent implements OnInit {
 
-  constructor() { }
+  dataRow = [];
+  public optionsTable:any[] = [
+    {
+      name: 'nro_docs',
+      text: 'N°',
+      typeField: 'text',
 
-  ngOnInit(): void {
+    },
+    {
+      name: 'numero_solicitud',
+      text: 'Número de negocio',
+      typeField: 'text',
+    },
+    {
+      name: 'cliente',
+      text: 'Nombre del cliente',
+      typeField: 'text',
+    },
+    {
+      name: 'identificacion',
+      text: 'CC del cliente',
+      typeField: 'text',
+      pipeName: 'number',
+    },
+    {
+      name: 'saldo_cartera',
+      text: 'Debito a cobrar',
+      typeField: 'text',
+      pipeName: 'number',
+    },
+    {
+      name: 'dias_mora',
+      text: 'Tramo de mora actual',
+      typeField: 'text',
+    },
+    {
+      name: 'lat',
+      text: 'Cuotas vencidad',
+      typeField: 'text',
+    },
+    {
+      name: 'vr_negocio',
+      text: 'Cuotas pendientes',
+      typeField: 'text',
+      pipeName: 'number',
+    },
+    {
+      name: 'lng',
+      text: 'Cuotas pagadas',
+      typeField: 'text',
+    }
+  ];
+  public displayedColumns: string[] = [
+    ...this.optionsTable.map(({ name }) => name),
+  ];
+  filtrarTabla: string="";
+
+  constructor(private _cobranzaService: CobranzaService,
+    private router: Router,
+    private _searchTableService: TableDataFilterService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getInformacionNegocios();
+  }
+  selecAlarmTable(e) {
+    console.log(e)
+    this.router.navigate([`/cobranza/asignacion-cuentas/${e.numeroSolicitud}`]);
+  }
+
+  getInformacionNegocios() {
+
+    this._cobranzaService.cuentasAsignadas$.subscribe((res) => {
+
+      this.dataRow = res;
+    });
+  }
+
+  public search(): void {
+    this._searchTableService.sendFilterData(this.filtrarTabla);
+  }
 }
